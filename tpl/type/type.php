edf22ebf923b641a0a70e001c2be50632d76c376
Juan Francisco Onielfa Veneros <paco@render.es>
2020-11-12 12:54:06 +0100

    Changes css load order of leaflet to avoid conflicts with page css

--- tpl/type/type.php
+++ tpl/type/type.php
@@ -4,7 +4,8 @@
 
 	// css
 		// page::$css_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/lib/jquery-ui/jquery-ui.min.css';
-		page::$css_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/lib/leaflet/leaflet.css';
+		// page::$css_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/lib/leaflet/leaflet.css';
+		array_unshift(page::$css_ar_url, __WEB_TEMPLATE_WEB__ . '/assets/lib/leaflet/leaflet.css');
 
 	// js
 		// page::$js_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/lib/jquery-ui/jquery-ui.min.js';

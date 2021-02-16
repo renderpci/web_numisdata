6801d4269b9fd73f048b18157192ab7a450eab87
Juan Francisco Onielfa Veneros <paco@render.es>
2021-02-16 09:56:36 +0100

    Fix leaflet css path issue

--- tpl/page/page.php
+++ tpl/page/page.php
@@ -17,6 +17,7 @@
 	# css
 		page::$css_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/css/main.css';
 		#page::$css_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/lib/bootstrap/css/bootstrap.min.css';
+		page::$css_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/lib/leaflet/leaflet.css';
 		page::$css_ar_url[] = __WEB_TEMPLATE_WEB__ . '/page/css/page.css';
 		#page::$css_ar_url[] = __WEB_TEMPLATE_WEB__ . '/menu/css/menu.css';
 

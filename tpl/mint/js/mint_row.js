a6c3f383f7be604209eb58e7aa8b7c53361c24b3
Jandro <jandro@render.es>
2022-03-03 12:23:00 +0100

    Mint page:Fixed coin size in print format

--- tpl/mint/js/mint_row.js
+++ tpl/mint/js/mint_row.js
@@ -190,6 +190,7 @@ var mint_row = {
 				parent			: img_link_ob
 			})
 			img_obverse.style.width = (diameter * 1) + 'mm'
+			img_obverse.style.height = (diameter * 1) + 'mm'
 			img_obverse.hires	= row.ref_coins_image_obverse
 			img_obverse.loading	= "lazy"
 			img_obverse.addEventListener("load", page.load_hires, false)
@@ -209,6 +210,7 @@ var mint_row = {
 				parent 			: img_link_re
 			})
 			img_reverse.style.width = (diameter* 1) + 'mm'
+			img_reverse.style.height = (diameter* 1) + 'mm'
 			img_reverse.hires	= row.ref_coins_image_reverse
 			img_reverse.loading	= "lazy"
 			img_reverse.addEventListener("load", page.load_hires, false)

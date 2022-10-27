4be3c2a855a82a087738aadc612bc9ccde408039
Julián Marrades <julian.marrades@hotmail.es>
2022-10-27 16:49:10 +0200

    Added a form structure to analysis
    TODO:
    1) Fill the form with fields
    2) Implement the submit form method

--- tpl/analysis/js/analysis.js
+++ tpl/analysis/js/analysis.js
@@ -7,12 +7,15 @@
 
 var analysis =  {
 
-
+	// Form factory instance
+	form: null,
 
 	area_name				: null,
-	export_data_container	: null,
 	row						: null,
 
+	// DOM containers
+	export_data_container	: null,
+	form_items_container	: null,
 
 
 	set_up : function(options) {
@@ -23,11 +26,111 @@ var analysis =  {
 			self.area_name				= options.area_name
 			self.export_data_container	= options.export_data_container
 			self.row					= options.row
+			self.form_items_container	= options.form_items_container
 
+		// form
+		const form_node = self.render_form()
+		self.form_items_container.appendChild(form_node)
 
 		return true
 	},//end set_up
 
+	/**
+	 * RENDER FORM
+	 */
+	render_form : function() {
+
+		const self = this
+
+		// DocumentFragment is like a virtual DOM
+		const fragment = new DocumentFragment()
+
+		// form_factory instance
+			self.form = self.form || new form_factory()
+
+		const form_row = common.create_dom_element({
+			element_type	: "div",
+			class_name		: "form-row fields",
+			parent			: fragment
+		})
+
+		// mint
+			self.form.item_factory({
+				id				: "mint",
+				name			: "mint",
+				label			: tstring.mint || "mint",
+				q_column		: "p_mint",
+				value_wrapper	: ['["', '"]'], // to obtain ["value"] in selected value only
+				eq				: "LIKE",
+				eq_in			: "%",
+				eq_out			: "%",
+				is_term			: true,
+				parent			: form_row,
+				callback		: function(form_item) {
+					self.form.activate_autocomplete({
+						form_item	: form_item,
+						table		: 'catalog'
+					})
+				}
+			})
+
+		// submit button
+			const submit_group = common.create_dom_element({
+				element_type	: "div",
+				class_name		: "form-group field button_submit",
+				parent			: fragment
+			})
+			const submit_button = common.create_dom_element({
+				element_type	: "input",
+				type			: "submit",
+				id				: "submit",
+				value			: tstring.search || "Search",
+				class_name		: "btn btn-light btn-block primary",
+				parent			: submit_group
+			})
+			submit_button.addEventListener("click", function (e) {
+				e.preventDefault()
+				self.form_submit(form)
+			})
+
+		// reset button
+			const reset_button = common.create_dom_element({
+				element_type	: "input",
+				type			: "button",
+				id				: "button_reset",
+				value			: tstring.reset || 'Reset',
+				class_name		: "btn btn-light btn-block secondary button_reset",
+				parent			: submit_group
+			})
+			reset_button.addEventListener("click", function (e) {
+				e.preventDefault()
+				window.location.replace(window.location.pathname);
+			})
+
+		// operators
+			// fragment.appendChild( forms.build_operators_node() )
+			const operators_node = self.form.build_operators_node()
+			fragment.appendChild( operators_node )
+
+		// the form element itself!
+			const form = common.create_dom_element({
+				element_type	: "form",
+				id				: "search_form",
+				class_name		: "form-inline"
+			})
+			form.appendChild(fragment)
+
+
+		return form
+	},//end render_form
+
+	/**
+	 * FORM SUBMIT
+	 * Form submit launch search
+	 */
+	form_submit : function(form_obj, options={}) {
+		// TODO: everything
+	}
 
 
 }//end analysis

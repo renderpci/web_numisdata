7c571b025f5064901183fb50604c6bea750fc765
Julián Marrades <julian.marrades@hotmail.es>
2022-10-31 13:59:36 +0100

    Added Chart.js library

--- tpl/page/page.php
+++ tpl/page/page.php
@@ -31,6 +31,7 @@
 		page::$js_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/lib/jquery-ui/jquery-ui.min.js';
 		page::$js_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/js/browser.min.js';
 		page::$js_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/js/breakpoints.min.js';
+		page::$js_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/js/chart.min.js';
 		page::$js_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/js/util'.JS_SUFFIX.'.js';
 		page::$js_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/js/main'.JS_SUFFIX.'.js';
 		//page::$js_ar_url[] = __WEB_TEMPLATE_WEB__ . '/assets/js/jquery.poptrox.min.js';
